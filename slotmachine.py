0d9199d741f25440d49847150c5046f6114d034d
Dalena <mimidalena@gmail.com>
2021-03-16 11:53:26 -0700

    More icons and venv

--- slotmachine.py
+++ slotmachine.py
@@ -1,5 +1,6 @@
 import random
 from enum import Enum
+import tkinter as tk
 
 
 class SlotMachine:
@@ -7,10 +8,16 @@ class SlotMachine:
 		self.money = money
 
 	class Symbols(Enum):
-		HEART = 1
-		DIAMOND = 2
-		SPADE = 3
-		CLOVER = 4
+		ACE = 1
+		KING = 2
+		QUEEN = 3
+		JACK = 4
+		JOKER = 5
+		HEART = 6
+		SPADE = 7
+		CLOVER = 8
+		DIAMOND = 9
+		
 
 	@property
 	def __continue_playing(self):
@@ -34,24 +41,24 @@ class SlotMachine:
 		#Roll slots
 		choices = [symbols.name for symbols in SlotMachine.Symbols]
 		first, second, third = random.choice(choices), random.choice(choices), random.choice(choices)
-
+		stars = 12 + len(first) + len(second) + len(third)
 		print(f"""	
-{'*'*35}
+{'*'*stars}
 *** {first} -- {second} -- {third} ***
-{'*'*35}
+{'*'*stars}
 		""")
 		self.__check_rolls(first, second, third)
 		
 	def __check_rolls(self, first, second, third):
 	#Check to see if rolls match
-		if first == second == third and first == "HEART":
-			self.money += 50
+		if first == second == third and first == "ACE":
+			self.money += 500
 			print("You matched all Hearts... you win $50\n")
 		elif first == second == third:
-			self.money += 5
+			self.money += 50
 			print("You matched 3 symbols... you win $5\n")
 		elif first == second or first == third or second == third:
-			self.money += 2
+			self.money += 20
 			print("You matched 2 symbols... you win $2\n")
 		else:
 			print("L-O-S-E-R")
@@ -64,7 +71,7 @@ class SlotMachine:
 			if self.money >= 2 and question:
 				question = self.__continue_playing
 				if question:
-					self.money -= 2
+					self.money -= 20
 					self.__play_round()
 			else:
 				print("\nThanks for playing!")
@@ -86,3 +93,12 @@ Otherwise... you get nothing!
 	# for symbol in SlotMachine.Symbols:
 	# 	print(symbol.name)
 	SlotMachine().play()
+'''
+Next  things to add:
+	-Change odds
+	-More icons and different payouts
+	-use graphics for rolls
+	-multiple lanes
+	-GUI with tkinter
+'''
+
